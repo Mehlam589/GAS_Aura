1328189161abe8169534b1d604946a5064e35989
Mehlam Bhanpurawala <mehlambhanpurawala@gmail.com>
2025-09-24 16:33:54 +0530

    AuraAbilityTypes Class Generation
    Created new class called AuraAbilityTypes.
    Created a new struct in it called FAuraGameplayEffectContext.
    
    Created 2 bools for BlockedHit and CriticalHit and there Getter and Setter Function.
    
    Also added 2 override functions from GameplayEffectTypes class:
    - GetScriptStruct
    - NetSerialize

---
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,19 @@
+// Copyright Flawky
+
+
+#include "AuraAbilityTypes.h"
+
+#include "GameplayEffectTypes.h"
+
+AuraAbilityTypes::AuraAbilityTypes()
+{
+}
+
+AuraAbilityTypes::~AuraAbilityTypes()
+{
+}
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return true;
+}
