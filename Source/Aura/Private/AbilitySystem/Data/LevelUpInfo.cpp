f7d2aba5d97ea877171bc230fb37f25c9a96f2fa
Mehlam Bhanpurawala <mehlambhanpurawala@gmail.com>
2025-12-26 11:48:27 +0530

    LevelupInfo for Level ups

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,5 @@
+// Copyright Flawky
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,24 @@
+// Copyright Flawky
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+
+
+};
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
